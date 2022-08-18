9f1593396bfd150b46add10ee42502b3395c72b1
khalid alasmary <khalidqp@gmail.com>
2022-08-18 15:35:15 +0300

    create context for videoPlayer and add active video style(not complete yet)

--- src/pages/courses/{ytbPlayList.title}.jsx
+++ src/pages/courses/{ytbPlayList.title}.jsx
@@ -1,16 +1,21 @@
 import { graphql } from "gatsby";
 import React from "react";
-import { VidoesList } from "../../components";
-import Youtube from 'react-youtube'
-import tw from "twin.macro";
+import { VideosList } from "../../components";
+import tw from "twin.macro";   
+import  { VideoPlayerProvider } from '../../context';
+import {VideoPlayer} from '../../components'
 
 const Course = ({ data }) => {
   const playlistItems = data.course.playlist.items;
+   const fristVidId =playlistItems[0].snippet.resourceId.videoId
+
   return (
-    <section tw="py-10 text-gray flex gap-38">
-      <VidoesList list={playlistItems} />
-      <Youtube videoId="X-KdDeUnRU4" opts={{width:500, hieght:450}}></Youtube>
+    <VideoPlayerProvider>
+    <section tw="py-10 text-gray flex gap-36">
+      <VideosList firstVidId={fristVidId} list={playlistItems} />     
+      <VideoPlayer/>  
     </section>
+  </VideoPlayerProvider>
   );
   //  <pre>{JSON.stringify(data.course.playlist.items, null, 4)}</pre>;
 };
