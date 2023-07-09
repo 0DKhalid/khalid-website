3b72c38793bb129c00c637d954c19e3c930e4644
khalid <dev.khalid.js@gmail.com>
2023-07-09 15:45:00 +0300

    currect the path for website favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,12 @@ module.exports = {
       },
     },
     {
+      resolve:'gatsby-plugin-manifest',
+      options:{
+        icon:`src/images/icon.png`
+      }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
