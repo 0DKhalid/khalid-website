9d6ec0083bb1c8ff1117e410bb342d636593b67a
khalid <dev.khalid.js@gmail.com>
2023-12-09 01:39:09 +0300

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-e9ea3f44b593c67a6a88.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkktech_website=self.webpackChunkktech_website||[]).push([[306],{51367:function(e,t,n){var u=n(64836);t.__esModule=!0,t.default=void 0;var r=u(n(7867)),a=u(n(80257)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return a.default.createElement(a.default.Fragment,null)},t}(a.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-e9ea3f44b593c67a6a88.js.map
